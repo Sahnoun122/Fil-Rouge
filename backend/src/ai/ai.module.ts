8f8a77b16e94a2977b4b89266b13b099a280b9e6
sahnoun122 <khadijasahnoun70@gmail.com>
2026-02-13 15:07:33 +0100

    feat: resoudre le probleme de l'import de fichies aiservice

--- backend/src/ai/ai.module.ts
+++ backend/src/ai/ai.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { AiService } from './ai.service';
+
 @Module({
   imports: [HttpModule],
   providers: [AiService],
